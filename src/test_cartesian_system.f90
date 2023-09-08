96d6e551f9e889f48d684733fb9aef4947bfc980
Derick Carnazzola <maxediv@gmail.com>
2023-09-08 11:24:47 -0500

    added Intel ifort debugging options and heading function/print

--- src/test_cartesian_system.f90
+++ src/test_cartesian_system.f90
@@ -1,8 +1,8 @@
 program test_cartesian_system
-use, non_intrinsic :: constants_m, only: rk, stdout, g0, ft2nmi
+use, non_intrinsic :: constants_m, only: rk, stdout, g0, ft2nmi, rad2deg
 use, non_intrinsic :: cartesian_system_m, only: cartesian_state, init_state
 use, non_intrinsic :: random_m, only: random_uniform, random_normal
-use, non_intrinsic :: vector_math_m, only: vmag
+use, non_intrinsic :: vector_math_m, only: vmag, heading
 use, non_intrinsic :: atmosphere_m, only: mach1
 implicit none
 
@@ -23,13 +23,14 @@ implicit none
     test = init_state(x_nmi=x_vals, y_nmi=y_vals, alt_ft=altitudes, heading_deg=headings, mach=machs)
 
     do i=1,n
-        write(stdout,'(a,i0,a)')                'Entity ',i,':'
-        write(stdout,'(a,3e14.6,a,2f7.1,f9.1)') '               Position [ft]: ',test(i)%x,', [nmi, nmi, kft]: ', &
-                                                                                 test(i)%x(1:2)*ft2nmi,test(i)%x(3)/1000.0_rk
-        write(stdout,'(a,3e14.6,a,f5.2)')       '           Velocity [ft/sec]: ',test(i)%v,',    Speed [mach]: ', &
-                                                                                 vmag(test(i)%v)/mach1(test(i)%x(3))
-        write(stdout,'(a,3e14.6,a,f5.2)')       '    Acceleration [ft/sec**2]: ',test(i)%a,',             G''s: ', &
-                                                                                 vmag(test(i)%a)/g0
+        write(stdout,'(a,i0,a)')                 'Entity ',i,':'
+        write(stdout,'(a,3e14.6,a,2f7.1,f9.1)')  '               Position [ft]: ',test(i)%x,', [nmi, nmi, kft]: ', &
+                                                 test(i)%x(1:2)*ft2nmi,test(i)%x(3)/1000.0_rk
+        write(stdout,'(a,3e14.6,a,f5.2,a,f6.1)') '           Velocity [ft/sec]: ',test(i)%v,',    Speed [mach]: ', &
+                                                 vmag(test(i)%v)/mach1(test(i)%x(3)),', Heading [deg]: ', &
+                                                 heading(test(i)%v(1:2))*rad2deg
+        write(stdout,'(a,3e14.6,a,f5.2)')        '    Acceleration [ft/sec**2]: ',test(i)%a,',             G''s: ', &
+                                                 vmag(test(i)%a)/g0
         write(stdout,'(a)') ''
     end do
 
--- src/vector_math_m.f90
+++ src/vector_math_m.f90
@@ -1,5 +1,5 @@
 module vector_math_m
-use, non_intrinsic :: constants_m, only: rk
+use, non_intrinsic :: constants_m, only: rk, twopi
 implicit none
 private
 
@@ -8,22 +8,29 @@ private
         module procedure vector_magnitude
     end interface vmag
 
-    public :: vmag
+    public :: vmag, heading
 
     contains
 
 
-        pure real(rk) function vec3_magnitude(x)
-            real(rk), intent(in) :: x(3)
-            vec3_magnitude = sqrt(x(1)*x(1) + x(2)*x(2) + x(3)*x(3))
+        pure real(rk) function vec3_magnitude(vec3)
+            real(rk), intent(in) :: vec3(3)
+            vec3_magnitude = sqrt(vec3(1)*vec3(1) + vec3(2)*vec3(2) + vec3(3)*vec3(3))
         end function vec3_magnitude
 
 
-        pure real(rk) function vector_magnitude(x, n)
-            real(rk), intent(in) :: x(*)
+        pure real(rk) function vector_magnitude(vec, n)
+            real(rk), intent(in) :: vec(*)
             integer, intent(in) :: n
-            vector_magnitude = sqrt(sum(x(1:n)**2))
+            vector_magnitude = sqrt(sum(vec(1:n)**2))
         end function vector_magnitude
+
+
+        pure real(rk) function heading(vec2)
+            real(rk), intent(in) :: vec2(2)
+            heading = atan2(vec2(1), vec2(2))
+            if (heading < 0.0_rk) heading = heading + twopi
+        end function heading
 
 
 end module vector_math_m
